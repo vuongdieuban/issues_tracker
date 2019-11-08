3b9e043dfc6c5905fd575165d5c3021fb873a95b
Ban Vuong <vuongdieuban@gmail.com>
2019-11-07 21:58:38 -0700

    add getOne,updateOne,deleteOne and validateObjectId middleware

--- backend/controllers/baseController.js
+++ backend/controllers/baseController.js
@@ -14,6 +14,28 @@ class BaseController {
     const data = await this.model.create(req.body);
     res.status(200).json(data);
   };
+
+  getOne = async (req, res) => {
+    const data = await this.model.findOne({ _id: req.params.id });
+    if (!data) return res.status(404).json("No data found with this id");
+    res.status(200).json(data);
+  };
+
+  updateOne = async (req, res) => {
+    const data = await this.model.findOneAndUpdate(
+      { _id: req.params.id },
+      req.body,
+      { new: true }
+    );
+    if (!data) return res.status(404).json("No data found with this id");
+    res.status(200).json(data);
+  };
+
+  deleteOne = async (req, res) => {
+    const data = await this.model.findOneAndRemove({ _id: req.params.id });
+    if (!data) return res.status(404).json("No data found with this id");
+    res.status(200).json(data);
+  };
 }
 
 module.exports = BaseController;
--- backend/routes/priority.js
+++ backend/routes/priority.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const router = express.Router();
+const validateObjectId = require("../middleware/validateObjectId");
 const PriorityController = require("../controllers/priority");
 
 router.get("/", PriorityController.getAll);
 router.post("/", PriorityController.post);
+router.get("/:id", validateObjectId, PriorityController.getOne);
+router.put("/:id", validateObjectId, PriorityController.updateOne);
+router.delete("/:id", validateObjectId, PriorityController.deleteOne);
 
 module.exports = router;
