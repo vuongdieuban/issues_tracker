c0c466de557bd1a59079d64d851f55680582a7e5
Ban Vuong <vuongdieuban@gmail.com>
2019-11-09 17:48:26 -0700

    init frontend boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "./App.css";
+
+function App() {
+  return (
+    <React.Fragment>
+      <div className="App">Hellow world</div>
+    </React.Fragment>
+  );
+}
+
+export default App;
