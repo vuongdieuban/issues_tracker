bdfb8c5d9408ab94d8d2135b3f585fedb6b24753
Ban Vuong <vuongdieuban@gmail.com>
2019-11-13 15:02:59 -0700

    fix backend middleware issueWriteAccess to query issue first to check with user._id instead of rely on openBy

--- backend/middleware/issueWiteAccess.js
+++ backend/middleware/issueWiteAccess.js
@@ -1,8 +1,11 @@
+const { Issue } = require("../models/issue");
 // IssueWiteAccess Middleware.
 // Check if user is the author of the issue (openBy) or user isAdmin
-module.exports = function(req, res, next) {
+module.exports = async function(req, res, next) {
+  const issue = await Issue.findOne({ _id: req.params.id });
+
   // req.user from auth middleware function, which should happens first before this middleware
-  if (req.user._id === req.body.openBy || req.user.isAdmin === true) {
+  if (req.user._id === issue.openBy.toString() || req.user.isAdmin === true) {
     next();
   } else {
     return res.status(403).send("Access Denied.");
