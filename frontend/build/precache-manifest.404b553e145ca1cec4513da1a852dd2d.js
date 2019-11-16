ea950ef66f2b108752ce05427a1fcc93873cd418
Ban Vuong <vuongdieuban@gmail.com>
2019-11-15 20:51:59 -0700

    build for prod

---
+++ frontend/build/precache-manifest.404b553e145ca1cec4513da1a852dd2d.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305e2b2f7c820f80c547f7468ac74ceb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "95ced34b26e5111704e0",
+    "url": "/static/css/2.3c8e31d9.chunk.css"
+  },
+  {
+    "revision": "f477982b27837caca79f",
+    "url": "/static/css/main.651e69f5.chunk.css"
+  },
+  {
+    "revision": "95ced34b26e5111704e0",
+    "url": "/static/js/2.563f7f3d.chunk.js"
+  },
+  {
+    "revision": "f477982b27837caca79f",
+    "url": "/static/js/main.e24a4385.chunk.js"
+  },
+  {
+    "revision": "742f1f7fc548cb268f56",
+    "url": "/static/js/runtime-main.3d4f50df.js"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "8880a65c57d7f031579335be153f64a0",
+    "url": "/static/media/marc.8880a65c.jpg"
+  },
+  {
+    "revision": "310509c95512893dc661bd3a6b0d2a5d",
+    "url": "/static/media/sidebar-2.310509c9.jpg"
+  }
+]);
